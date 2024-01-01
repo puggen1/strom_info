69144bb9801fdcb9d041a34ef85e6bc44a23766a
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2024-01-01 16:00:52 +0100

    found another fix for image trouble

--- components/singleArticleSection/index.jsx
+++ components/singleArticleSection/index.jsx
@@ -2,6 +2,8 @@ import { Image, View,Text, Dimensions  } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import colors from '../../assets/style/colors'
 import text from '../../assets/style/text'
+import ResponsiveImageView from 'react-native-responsive-image-view';
+
 const SingleArticleSection = ({data}) => {
   if(data.children){
     if(data.style == "h4"){
@@ -25,7 +27,14 @@ const SingleArticleSection = ({data}) => {
   }
   else if(data._type === "image"){
     let width = Dimensions.get("window").width - 65
-    return <View style={{width:"100%", marginVertical:15}}><Image style={{marginLeft:"auto", marginRight:"auto"}} source={{uri:data.imageUrl}}  width={width} /></View>
+    return <View style={{width:"100%", marginVertical:15}}>
+      <ResponsiveImageView  source={{uri:data.imageUrl}}>
+      {({ getViewProps, getImageProps }) => (
+        <View {...getViewProps()}>
+          <Image {...getImageProps()} />
+        </View>
+      )}
+      </ResponsiveImageView></View>
 
   }
 }
